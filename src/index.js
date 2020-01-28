eadf3ad77f9a8bd08131eb5b07bb5795a702cea8
sidntrivedi012 <sidntrivedi012@gmail.com>
2020-01-28 12:30:24 +0530

    #2 Query to fetch feed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
 type Query{
     info: String!
+    feed: [Link!]!
+}
+
+type Link{
+    id: ID!
+    url: String!
+    description: String!
 }`;
 
+let links = [
+  {
+    id: "link-0",
+    url: "https://osdc.netlify.com",
+    description: "OSDC Website"
+  }
+];
 const resolvers = {
   Query: {
-    info: () => `This is hackernews clone.`
+    info: () => `This is hackernews clone.`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    url: parent => parent.url,
+    description: parent => parent.description
   }
 };
 
